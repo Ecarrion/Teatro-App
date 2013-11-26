ad040a93b0014d762f6e64375cb6e791089e4fbf
Ernesto Carrión <carrion.silver@gmail.com>
2013-11-25 22:16:15 -0500

    Launch Lock screen everytime app is launched

--- Hotel App/AppDelegate.m
+++ Hotel App/AppDelegate.m
@@ -40,6 +40,12 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    if (!self.window.rootViewController.presentedViewController) {
+        
+        LockscreenViewController * lvc = [[LockscreenViewController alloc] init];
+        [self.window.rootViewController presentViewController:lvc animated:NO completion:nil];
+    }
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
